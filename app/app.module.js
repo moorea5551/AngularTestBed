da87103002dcd5a17506aba6375050a95bb3ef9d
moorea5551 <moorea5551@gmail.com>
2015-02-27 21:07:31 -0600

    adding closet tracking functionality outputting description and color onto home page

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,11 @@
 var app = angular.module('index', [ ]);
+
+app.controller('ClosetController', ['$http', function($http) {
+    this.closetData = [];
+
+    // apply closet data from json or WS
+    var closet = this;
+    $http.get("assets/js/closetData.json").success(function (data) {
+        closet.closetData = data;
+    });
+}]);
